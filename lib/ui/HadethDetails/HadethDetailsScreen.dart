38a272f899f2400281b7d684c25d9b4e0fa84091
elshimy911 <temoeslam25@gmail.com>
2025-02-28 15:59:01 +0200

    + feature hadeth

---
+++ lib/ui/HadethDetails/HadethDetailsScreen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/ui/home/hadeth/Hadeth.dart';
+
+class HadethDetailsScreen extends StatelessWidget {
+  static const String routeName = 'Hadeth-Deatails-Screen';
+  @override
+  Widget build(BuildContext context) {
+    var args = ModalRoute.of(context)?.settings.arguments as Hadeth;
+    return Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/default_bg.png'),
+                fit: BoxFit.fill)),
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(args.title),
+            ),
+            body:Column(
+              children: [
+                Expanded(
+                  child: Card(
+                    margin: EdgeInsets.symmetric(vertical: 48, horizontal: 24),
+                    child: SingleChildScrollView(
+                      child: Text(
+                        args.content,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                                  ),
+                    ),
+                  
+                  ),
+                ),
+              ],
+            )));
+  }
+}
